d905bc27b8ccae917b25ee5dce8cab9b93863ec4
spacebroth <85671590+spacebroth@users.noreply.github.com>
2022-08-30 10:23:04 +0300

    Update disk_page.py

--- pages/disk_page.py
+++ pages/disk_page.py
@@ -10,9 +10,6 @@ class DiskPage(SeleniumBase):
         super().__init__(driver)
         self.driver = driver
 
-    # Понимаю, что иногда появляется подтверждение телефона, как его обойти точно не знаю. Было две мысли:
-    # 1. Использовать определенные куки другой сессии и загрузить их в текущую
-    # 2. Как-то замокать, сделать "заглушку" этого запроса
     def authorization_from_main_page(self):
         self.is_clickable('css', _.enter_button_selector, 'Кнопка "Войти"').click()
         self.is_clickable('css', _.mail_login_selector, 'Переключатель способа логина').click()
