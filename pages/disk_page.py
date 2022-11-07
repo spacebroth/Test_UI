c38b5456ce9477225482c102f65844cdf47a75d6
spacebroth <85671590+spacebroth@users.noreply.github.com>
2022-11-07 06:40:03 +0300

    Update disk_page.py

--- pages/disk_page.py
+++ pages/disk_page.py
@@ -1,3 +1,4 @@
+import time
 import pages.selectors as _
 from selenium.webdriver import Keys
 from typing import List
@@ -10,25 +11,17 @@ class DiskPage(SeleniumBase):
         super().__init__(driver)
         self.driver = driver
 
-    def authorization_from_main_page(self):
-        self.is_clickable('css', _.enter_button_selector, 'Кнопка "Войти"').click()
-        self.is_clickable('css', _.mail_login_selector, 'Переключатель способа логина').click()
-        self.is_clickable('css', _.login_input_selector, 'Поле ввода логина').send_keys('popotoro154', Keys.ENTER)
-        self.is_clickable('css', _.password_input_selector, 'Поле ввода пароля').send_keys('ZXCasdQWE123_', Keys.ENTER)
-
-    def go_to_disk(self):
-        self.is_clickable('css', _.disk_button_in_card_menu_selector, 'Ссылка "Диск"').click()
-        self.driver.switch_to.window(self.driver.window_handles[-1])
-
     def create_new_item_with_name_(self, item: str, item_name: str):
         self.is_clickable('css', _.create_button_selector, 'Кнопка "Создать"').click()
         self.get_link_by_name(item, _.creatable_items_selector).click()
         modal_input = self.is_clickable('css', _.create_item_input_selector, 'Поле ввода имени создаваемой сущности')
+        time.sleep(0.5)
         modal_input.send_keys(item_name, Keys.ENTER)
 
     def copy_or_move_item(self, action: str, where: str):
         actions = ActionChains(self.driver)
         actions.double_click(self.get_link_by_name('Общая папка', _.listing_items_selector)).perform()
+        time.sleep(0.5)
         actions.context_click(self.get_link_by_name('Файл для копирования.docx', _.listing_items_selector)).perform()
         self.get_link_by_name(action, _.context_menu_selector).click()
         self.get_link_by_name(where, _.modal_elements_selector).click()
@@ -52,6 +45,6 @@ class DiskPage(SeleniumBase):
         return element
 
     def sign_out(self):
-        self.is_clickable('css', _.user_header_button_selector, 'Кнопка аватар юзера').click()
-        self.get_link_by_name('Выйти', _.user_menu_links_selector).click()
+        self.is_clickable('css', _.user_header_button_in_disk_selector, 'Кнопка аватар юзера').click()
+        self.get_link_by_name('Выйти', _.user_menu_links_in_disk_selector).click()
         self.driver.switch_to.alert.accept()
