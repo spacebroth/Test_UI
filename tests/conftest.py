e87b9c7d67d1ce0f1f9e35b781dbd9a4c084969e
spacebroth <85671590+spacebroth@users.noreply.github.com>
2022-11-07 06:37:55 +0300

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,25 +3,29 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as chrome_options
 
 
-@pytest.fixture
-def get_chrome_options():
-    options = chrome_options()
-    options.add_argument('--window-size=1920,1080')
-    options.add_argument('--headless')  # "--headless"
-    return options
+# @pytest.fixture
+# def get_chrome_options():
+#     options = chrome_options()
+#     options.add_argument('--window-size=1920,1080')
+#     options.add_argument('chrome')  # "--headless"
+#     driver = webdriver.Chrome(options=options)
+#     url = 'https://ya.ru/'
 
 
-@pytest.fixture
-def get_webdriver(get_chrome_options):
-    options = get_chrome_options
-    driver = webdriver.Chrome(options=options)
-    return driver
+# @pytest.fixture
+# def get_webdriver(get_chrome_options):
+#     options = get_chrome_options
+#     driver = webdriver.Chrome(options=options)
+#     return driver
 
 
 @pytest.fixture(scope='function')
-def setup(request, get_webdriver):
-    driver = get_webdriver
-    url = 'https://yandex.ru/'
+def setup(request):
+    options = chrome_options()
+    options.add_argument('--window-size=1920,1080')
+    options.add_argument('chrome')  # "--headless"
+    driver = webdriver.Chrome(options=options)
+    url = 'https://ya.ru/'
     if request.cls is not None:
         request.cls.driver = driver
     driver.get(url)
