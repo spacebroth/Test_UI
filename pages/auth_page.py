9c5e5405d4e38df19659b3c93951b237ff140db5
spacebroth <85671590+spacebroth@users.noreply.github.com>
2022-11-11 21:00:10 +0300

    Create auth_page.py

---
+++ pages/auth_page.py
@@ -0,0 +1,16 @@
+import pages.selectors as _
+from selenium.webdriver import Keys
+from base.base_class import SeleniumBase
+
+
+class AuthPage(SeleniumBase):
+    def __init__(self, driver):
+        super().__init__(driver)
+        self.driver = driver
+
+
+    def authorization_from_main_page(self):
+        self.is_clickable('css', _.enter_button_selector, 'Кнопка "Войти"').click()
+        self.is_clickable('css', _.mail_login_selector, 'Переключатель способа логина').click()
+        self.is_clickable('css', _.login_input_selector, 'Поле ввода логина').send_keys('ЛОГИН', Keys.ENTER)
+        self.is_clickable('css', _.password_input_selector, 'Поле ввода пароля').send_keys('ПАРОЛЬ', Keys.ENTER)
